565e0b962ccb5a3a6b547788756b358a41c30c24
Deependra Shrestha <36838592+dpenra@users.noreply.github.com>
2024-11-21 23:43:09 +0545

    Update android15.sh

--- android15.sh
+++ android15.sh
@@ -12,7 +12,6 @@ rm -rf hardware/qcom-caf/sm8150/media \
        packages/apps/Updater \
        vendor/lineage
 
-
 git clone https://github.com/yaap/hardware_qcom-caf_sm8150_media.git --depth 1 -b fifteen hardware/qcom-caf/sm8150/media
 git clone https://github.com/dpenra/android_packages_apps_Updater.git --depth 1 -b fifteen packages/apps/Updater
 git clone https://github.com/dpenra/android_vendor_lineage.git --depth 1 -b fifteen vendor/lineage

e63f40cfd45a44296b82dd665b4b41b27530af63
Deependra Shrestha <36838592+dpenra@users.noreply.github.com>
2024-11-21 23:51:51 +0545

    Update android15.sh

--- android15.sh
+++ android15.sh
@@ -4,14 +4,10 @@
 # set -e
 
 repo init -u https://github.com/RisingTechOSS/android -b fifteen --git-lfs
-/opt/crave/resync.sh
-
-echo "### Cleaning up existing directories..."
-# Remove existing directories to avoid conflicts
-rm -rf hardware/qcom-caf/sm8150/media \
-       packages/apps/Updater \
-       vendor/lineage
-
+# /opt/crave/resync.sh
+rm -rf hardware/qcom-caf/sm8150/media
+rm -rf packages/apps/Updater
+rm -rf vendor/lineage
 git clone https://github.com/yaap/hardware_qcom-caf_sm8150_media.git --depth 1 -b fifteen hardware/qcom-caf/sm8150/media
 git clone https://github.com/dpenra/android_packages_apps_Updater.git --depth 1 -b fifteen packages/apps/Updater
 git clone https://github.com/dpenra/android_vendor_lineage.git --depth 1 -b fifteen vendor/lineage

23546b27ee6600b58efbc378428dcd1765153400
Deependra Shrestha <36838592+dpenra@users.noreply.github.com>
2024-11-24 21:15:45 +0545

    Update android15.sh

--- android15.sh
+++ android15.sh
@@ -2,12 +2,8 @@
 
 repo init -u https://github.com/RisingTechOSS/android -b fifteen --git-lfs
 /opt/crave/resync.sh
-rm -rf hardware/qcom-caf/sm8150/media
-rm -rf packages/apps/Updater
-rm -rf vendor/lineage
-git clone https://github.com/yaap/hardware_qcom-caf_sm8150_media.git --depth 1 -b fifteen hardware/qcom-caf/sm8150/media
-git clone https://github.com/dpenra/android_packages_apps_Updater.git --depth 1 -b fifteen packages/apps/Updater
-git clone https://github.com/dpenra/android_vendor_lineage.git --depth 1 -b fifteen vendor/lineage
+chmod +x android.sh
+./android.sh
 . build/envsetup.sh
 riseup sunny user
 rise b
